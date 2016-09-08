d39d7d99a4ec580f1656a0b97ae99c74bcb68321
wangkaihua123 <1310321307@qq.com>
2016-09-08 14:00:57 +0800

    Update classie.js

--- moban809/js/classie.js
+++ moban809/js/classie.js
@@ -15,7 +15,7 @@
 
 'use strict';
 
-// class helper functions from bonzo https://github.com/ded/bonzo
+// class helper functions from bonzo 
 
 function classReg( className ) {
   return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
